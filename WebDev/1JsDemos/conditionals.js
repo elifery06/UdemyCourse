edbd2bbe13b7f4a722fa78f82dbd1a7883ac5e51
ELİF ERYILDIRIM <75325359+elifery06@users.noreply.github.com>
2022-01-06 14:35:06 +0300

    Add files via upload

---
+++ WebDev/1JsDemos/conditionals.js
@@ -0,0 +1,15 @@
+var not=4 
+if (not>=50) {
+    console.log('geçtin')
+    
+}
+else if(not>=40){
+    console.log('bütünlemeye kaldın')
+}
+// if (not>=50) {
+//     console.log('geçtinizz')
+    
+// }
+else{
+    console.log('kaldınız')
+}
