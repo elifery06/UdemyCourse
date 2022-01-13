a32e8b235e1b344e9c9893c009324f271e049d3c
ELİF ERYILDIRIM <75325359+elifery06@users.noreply.github.com>
2022-01-13 10:21:24 +0300

    Add files via upload

---
+++ WebDev/WebDev/1JsDemos/conditionalsDemo.js
@@ -0,0 +1,29 @@
+var sayi1 = 29
+var sayi2 = 30
+var sayi3 = 2
+
+//En büyük sayıyı bul
+
+//  if (sayi1>sayi2 && sayi1>sayi3) {
+//      console.log('en büyük sayı= '+sayi1)
+
+//  }
+//  else if(sayi2>sayi1 && sayi2>sayi3){
+//     console.log('en büyük sayı= '+sayi2)
+
+
+//  }
+//  else{
+//     console.log('en büyük sayı= '+sayi3);
+
+//  }
+
+var enBuyuk = sayi1
+if (enBuyuk < sayi2) {
+    enBuyuk = sayi2
+}
+if (enBuyuk < sayi3) {
+    enBuyuk = sayi3
+
+}
+     console.log('en büyük sayı= ',enBuyuk);
